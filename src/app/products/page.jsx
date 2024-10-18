1d1bf3b214da5d1c13543cbcd412e6f114881886
Nafiur01 <nafiurrahman373@gmail.com>
2024-10-18 23:51:03 +0600

    Data Caching

---
+++ src/app/products/page.jsx
@@ -0,0 +1,32 @@
+import { cookies } from "next/headers";
+
+export const fetchCache = "default-cache";
+
+import React from "react";
+
+export default async function ProductPage() {
+  const response = await fetch("http://localhost:3001/products");
+  const products = await response.json();
+
+  const cookieStore = cookies();
+  cookieStore.get("theme");
+
+  const detailResponse = await fetch("http://localhost:3001/products/1");
+  const details = await detailResponse.json();
+
+  return (
+    <ul className="space-y-4 p-4">
+      {products.map((product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+          <p>{details.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { resolve } from "styled-jsx/css";
 
 export default async function UsersPage() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users12");
   const user = await response.json();
   console.log(user);
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 gap-2 p-4">
       {user.map((item, index) => {
         return (
-          <div className="flex flex-row gap-2 justify-center" key={index}>
-            <h1>{item.id}</h1>
-            <p>{item.name} <span className="font-bold">username</span> {item.username}</p>
+          <div
+            className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-gray-600"
+            key={item.id}
+          >
+            <div className="flex flex-col space-y-1">
+              <h2 className="text-lg font-semibold">{item.name}</h2>
+              <p className="text-sm ">{item.username}</p>
+            </div>
+            <div className="flex flex-col space-y-1 items-end">
+              <div className="text-md">{item.email}</div>
+              <div className="text-md">{item.phone}</div>
+            </div>
           </div>
         );
       })}
